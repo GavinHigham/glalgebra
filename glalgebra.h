9b28ecf4f4137de910a443ea2eeef406b90e84ea
GavinHigham <Gavin.Higham@gmail.com>
2016-06-01 21:25:44 -0700

    Added MIT license because of a thing I read on Reddit

--- glalgebra.h
+++ glalgebra.h
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2016 Gavin Higham
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #ifndef GLALGEBRA_H
 #define GLALGEBRA_H
 
@@ -69,7 +93,7 @@ mat3 mat3_mult(mat3 a, mat3 b);
 vec3 mat3_multvec(mat3 a, vec3 b);
 //Rotate a about <ux, uy, uz> by some angle, provided by s and c.
 //s and c should be the sine and cosine of the angle, respectively.
-mat3 mat3_rotmat(float ux, float uy, float uz, float s, float c);
+mat3 mat3_rot(mat3 a, float ux, float uy, float uz, float s, float c);
 //Produces the matrix for a rotation about <ux, uy, uz> by some angle, provided by s and c.
 //s and c should be the sine and cosine of the angle, respectively.
 mat3 mat3_rotmat(float ux, float uy, float uz, float s, float c);
